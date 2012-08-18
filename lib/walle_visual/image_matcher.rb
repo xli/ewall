4ba0547b1f0aab72384685c8e75f6a33da3d00f6
Xiao Li <swing1979@gmail.com>
2012-08-17 23:41:35 -0700

    update image_matcher to use new match_descriptors api

--- lib/walle_visual/image_matcher.rb
+++ lib/walle_visual/image_matcher.rb
@@ -9,7 +9,7 @@ module WalleVisual
     # find image matched with given file and return the index of the image in the image files
     def match(image_file)
       image = load_as_gray_image(image_file)
-      matches = image.match_descriptors("SURF", "SURF", "FlannBased", @images)
+      matches = image.match_descriptors(@images)
       if m = matches.max_by{|_, count| count}
         m[0]
       end
