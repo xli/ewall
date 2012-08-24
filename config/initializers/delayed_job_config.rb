e73f32ac6b77efbf67479980f68b4ffb0f1c6d53
Xiao Li <swing1979@gmail.com>
2012-08-24 09:43:00 -0700

    do not execute delayed job in test env

--- config/initializers/delayed_job_config.rb
+++ config/initializers/delayed_job_config.rb
@@ -3,4 +3,4 @@ Delayed::Worker.sleep_delay = 6
 Delayed::Worker.max_attempts = 1
 Delayed::Worker.max_run_time = 20.minutes
 Delayed::Worker.read_ahead = 10
-Delayed::Worker.delay_jobs = !Rails.env.test?
+Delayed::Worker.delay_jobs = true
